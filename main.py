90dfd4725daea3a53e3efb618c20e3479e501176
Aizhan <aizhan1maks@gmail.com>
2026-06-09 22:22:27 +0800

    Added healthcheck endpoint for verifying if the ml model is loaded.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from schemas import PredictionInput, PredictionOutput
 from contextlib import asynccontextmanager
 from ml_model.pre_process import preprocess_input
@@ -20,3 +20,11 @@ async def submit_prediction(input_data: PredictionInput):
     input_data = preprocess_input(input_data)
     prediction = Predictor().predict(input_data, app.state.prediction_model)
     return prediction
+
+
+@app.get("/health")
+def health_check():
+    if app.state.prediction_model is not None:
+        return {"status": status.HTTP_200_OK, "message": "Model is loaded and ready to use."}
+    else:
+        return {"status": status.HTTP_404_NOT_FOUND, "message": "Model is not loaded."}
