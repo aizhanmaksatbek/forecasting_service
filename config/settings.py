bd12d0fc9ac08201c22265753e04c5c2c8546f4d
Aizhan <aizhan1maks@gmail.com>
2026-06-14 23:47:02 +0800

    Updated config settings

--- config/settings.py
+++ config/settings.py
@@ -2,17 +2,8 @@ FUTURE_HORIZON = 28
 PAST_DAYS = 1
 
 DATA_FILE_PATH = "data/panel.csv"
-ML_MODEL_DIR = "checkpoints/"
-PRODUCT_NAME = "BEVERAGES"
-STORE_NUMBER = 1
+ML_MODEL_CHECKPOINT = "checkpoints/tft_best_train_final.pt"
 
-
-"""Configuration settings for TFT model."""
-# WORKING_DIR = "/kaggle/input/demandForecasting"
-WORKING_DIR = ""
-RAW = f"{WORKING_DIR}/data/"
-
-# encoder features
 ENC_VARS = [
     "sales",
     "transactions",
@@ -24,7 +15,7 @@ ENC_VARS = [
     "is_holiday",
     "is_workday"
 ]
-# known future features
+
 DEC_VARS = [
     "onpromotion",
     "dow",
@@ -33,7 +24,7 @@ DEC_VARS = [
     "is_holiday",
     "is_workday"
 ]
-# static features
+
 STATIC_COLS = [
     "store_nbr",
     "family",
@@ -45,10 +36,3 @@ REALS_TO_SCALE = [
     "transactions",
     "dcoilwtico"
     ]
-
-
-import os
-
-
-WORKING_DIR = ""
-TFT_CHECKPOINTS_DIR = os.path.join(WORKING_DIR, "TFT", "checkpoints")
