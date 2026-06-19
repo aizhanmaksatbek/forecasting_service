f8ae7bae33c013c53722f8af4fffaa9f9b764891
Aizhan <aizhan1maks@gmail.com>
2026-06-19 16:18:49 +0800

    Pasted make_forecast() into predictor.py

--- schemas/routers.py
+++ schemas/routers.py
@@ -1,16 +1,18 @@
-from schemas.schemas import PredictionOutput
 from fastapi import APIRouter, Depends
 from ml_model.data_prep import get_input_data
 from ml_model.predictor import Predictor
 from typing import Annotated
 import pandas as pd
+from fastapi import BackgroundTasks
+
 
 router = APIRouter()
 
 
-@router.post("/model", response_model=PredictionOutput)
+@router.post("/model")
 async def submit_prediction(
     input_data: Annotated[pd.DataFrame, Depends(get_input_data)],
+    background_task: BackgroundTasks
 ):
-    prediction = Predictor().predict(input_data)
-    return prediction
+    background_task.add_task(Predictor().predict, input_data)
+    return {"message": "Started forecasting"}
